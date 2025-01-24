78f59959a97e90a8de1a798971f8afcbaaec773d
agarnung <garnungalejandro@gmail.com>
2025-01-24 01:56:21 +0100

    Trying to disable cacheying

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,16 @@ import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [svelte()],
+  output: 'static',
+  site: 'https://web-riddler.vercel.app/',
+  server: {
+    // Server configuration
+    headers: {
+      // Cache control to prevent caching
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+    },
+  },
+
 });
 
