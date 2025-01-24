4576d0eeb04c3874be3dee0b45886d234d370ecd
agarnung <garnungalejandro@gmail.com>
2025-01-24 18:12:07 +0100

    errores infernales solucionados suprimiendo client:load duplicado

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
-import vercelServerless from '@astrojs/vercel/serverless'; // https://github.com/withastro/astro/issues/4464
-import dotenv from 'dotenv';
+import vercel from '@astrojs/vercel';
 
+import dotenv from 'dotenv';
 dotenv.config();
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [svelte()],
+  
   output: 'server',
-  adapter: vercelServerless({
-    imageService: true, 
-  }),
-});
+  adapter: vercel(),
 
+  server: {
+    // Server configuration
+    headers: {
+      // Cache control to prevent caching
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+    },
+  },
+});
